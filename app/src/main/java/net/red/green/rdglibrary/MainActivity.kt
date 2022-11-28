06c0ddf36a09a92d97a24be1e217398433121645
jubayar <jubayar.ict@gmail.com>
2022-11-28 09:09:39 +0800

    Release new lib version 1.0.2

--- app/src/main/java/net/red/green/rdglibrary/MainActivity.kt
+++ app/src/main/java/net/red/green/rdglibrary/MainActivity.kt
@@ -1,8 +1,10 @@
 package net.red.green.rdglibrary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import net.red.green.rdgmath.MathActivity
 import net.red.green.rdgmath.MathHelper
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textview = findViewById<TextView>(R.id.msg)
-        textview.text = "2 + 5 = " + MathHelper().addition(2, 5)
+        //val textview = findViewById<TextView>(R.id.msg)
+        //textview.text = "2 + 5 = " + MathHelper().addition(2, 5)
+
+        val intent = Intent(this, MathActivity::class.java)
+        startActivity(intent)
     }
 }
