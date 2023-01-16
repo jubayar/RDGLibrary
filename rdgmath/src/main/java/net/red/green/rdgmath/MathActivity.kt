2da84c56859bdd3ca24c77bcf96f1dbe64652341
jubayar <jubayar.ict@gmail.com>
2023-01-17 06:55:40 +0800

    Configure notimsg module for lib

--- rdgmath/src/main/java/net/red/green/rdgmath/MathActivity.kt
+++ rdgmath/src/main/java/net/red/green/rdgmath/MathActivity.kt
@@ -2,11 +2,16 @@ package net.red.green.rdgmath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import net.red.green.rdgnoti.NotiMsg
 
 class MathActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_math)
+
+        val msg = NotiMsg()
+        Toast.makeText(this, msg.showMsg(), Toast.LENGTH_SHORT).show()
     }
 }
